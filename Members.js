eee54e1e3cdbf27c2bfd75f418436f87f284e869
Abhishek Sahani <abhisheksahani653@gmail.com>
2023-03-26 22:21:54 +0530

    Create get api members and delete member

---
+++ Members.js
@@ -0,0 +1,9 @@
+const members = [
+  { id: "1", name: "Start writing for 10mins a day" },
+  { id: "2", name: "Exercise daily for minimum 30mins" },
+  { id: "3", name: "Meditation regularly" },
+  { id: "4", name: "Travelling once a month" },
+  { id: "5", name: "Coding everyday" },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const Members = require("./Members");
 
 const PORT = process.env.PORT || "8000";
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
+});
+
+// Get all members
+
+app.get("/todos", (req, res) => {
+  res.json(Members);
+});
+
+//Get members based on id
+
+app.get("/todos/:id", (req, res) => {
+  console.log(req.params.id, typeof req.params.id, "dhsh");
+  res.json(Members.filter((member) => member.id === req.params.id));
+});
+
+//Delete todo based on id
+
+app.delete("/:id", (req, res) => {
+  res.json(Members.filter((member) => member.id !== req.params.id));
 });
 
 app.listen(PORT, () => console.log(`Server is running at PORT:${PORT}`));
